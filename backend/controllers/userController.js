14bb8ca25a464d3a70d19da562b8795a6ca2f912
locodafux <locodafux@gmail.com>
2024-01-25 21:05:30 +0800

    update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,13 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 // @desc Auth user/set token
 //route POST /api/users/auth
 //@access Public
 const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
   res.status(200).json({ message: "Auth user" });
 });
 
@@ -11,8 +15,32 @@ const authUser = asyncHandler(async (req, res) => {
 //route POST /api/users/
 //@access Public
 const registerUser = asyncHandler(async (req, res) => {
-  console.log(req.body);
-  res.status(200).json({ message: "Register user" });
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email: email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User data");
+  }
 });
 
 // @desc Logout user
